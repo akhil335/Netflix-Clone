c2bfeebbc1a4f1335df6bf2af20bf401ac750cd4
hikkigaya335 <akhilsemwal05@gmail.com>
2023-01-18 22:20:54 +0530

    card ui made

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 
 export default function CardSlider({data, title}) {
-    return <div>
+    return <div className="flex">
         {
             data.map((movie, index) => {
                 return <Card movieData={movie} index={index} key={movie.id}></Card>
