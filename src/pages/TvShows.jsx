31adaafcc5c0024fa04e676176b8e89956bd5360
hikkigaya335 <akhilsemwal05@gmail.com>
2023-01-22 22:12:10 +0530

    fetch category imported

--- src/pages/TvShows.jsx
+++ src/pages/TvShows.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchMovieList, getGenres } from "../store";
+import { fetchCategory, getGenres } from "../store";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
